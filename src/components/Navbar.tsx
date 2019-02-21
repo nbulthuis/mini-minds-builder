cba3434c2638037af8bcd2e1a2a41c27f6d9b8be
Nate Bulthuis <nate.bulthuis@gmail.com>
2019-02-20 20:51:34 -0500

    fix dependency vulnerability

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 
-const Navbar: React.FunctionComponent<{user?: string}> = ({user: "Guest"}) => {
-  const [timer, setTimer] = useState(0);
+const Navbar = () => {
   
   return(
     <>
